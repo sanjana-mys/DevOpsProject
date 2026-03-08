82170e78f5a1824f12dace301d802cebde5a5ec8
sanjana-mys <145591957+sanjana-mys@users.noreply.github.com>
2026-03-08 15:44:49 +0530

    Correct sum calculation and output in Hello.java
    Fix syntax error in sum calculation and print statement.

--- Hello.java
+++ Hello.java
@@ -2,5 +2,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from DevOps Jenkins Pipeline");
         System.out.println("Hello Sanjanaaa");
+        int a=1;
+        int b=2;
+        int sum a+b;
+        System.out.println("%d",sum);
     }
 }

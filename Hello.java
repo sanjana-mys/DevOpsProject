04186ef3d91408adad5c6b5f156e492490aba7c3
sanjana-mys <145591957+sanjana-mys@users.noreply.github.com>
2026-03-08 15:41:57 +0530

    Add greeting for Sanjanaaa in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from DevOps Jenkins Pipeline");
+        System.out.println("Hello Sanjanaaa");
     }
 }

32ee38dc0c1379ab00675211192311fc27653a07
sanjana-mys <145591957+sanjana-mys@users.noreply.github.com>
2026-03-09 09:10:47 +0530

    Add greeting for DevOps Lab in Hello.java
    Added a new greeting message for DevOps Lab.

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from DevOps Jenkins Pipeline");
         System.out.println("Hello Sanjanaaa");
+        System.out.println("Hello DevOps Lab");
+
         int a=1;
         int b=2;
         int sum a+b;

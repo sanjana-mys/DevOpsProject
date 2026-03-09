a527db097b1ccc8df2b7b37c974264018daacbee
sanjana-mys <145591957+sanjana-mys@users.noreply.github.com>
2026-03-09 09:14:41 +0530

    Add greeting and fix sum calculation in Hello.java
    Fix syntax error in sum calculation and add greeting.

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
         System.out.println("Hello from DevOps Jenkins Pipeline");
         System.out.println("Hello Sanjanaaa");
         System.out.println("Hello DevOps Lab");
-
+        System.out.println("Hiiiiiii");   
         int a=1;
         int b=2;
         int sum a+b;
